d4242ba75c1df1c66f0c6ba4d96606490a2330c0
Brad Hodkinson <hodkinbr@amazon.com>
2022-12-03 19:16:32 -0800

    Added a template Answer class

---
+++ 2022/src/Template/Answer.java
@@ -0,0 +1,27 @@
+package src.Template;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import src.Solution;
+
+public class Answer extends Solution {
+
+    public Answer() {
+        super(true);
+    }
+
+    @Override
+    protected
+    void solve(BufferedReader reader) throws IOException {
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            
+        }
+        
+        System.out.println("Part 1: "); // ans 1
+        System.out.println("Part 2: "); // ans 2
+    }
+
+}
